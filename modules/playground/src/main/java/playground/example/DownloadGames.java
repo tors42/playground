60c5b0e669749f1fc86e35fca3335c36e41da255
tors42 <4084220+tors42@users.noreply.github.com>
2023-01-27 19:47:24 +0100

    Use chariot-0.0.59 with PGN instead of JSON
    The Game modelling with pgnInJson parameter is more expensive serverside
    than asking for the PGNs (application/x-chess-pgn).

--- modules/playground/src/main/java/playground/example/DownloadGames.java
+++ modules/playground/src/main/java/playground/example/DownloadGames.java
@@ -6,6 +6,7 @@ import java.nio.file.*;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Consumer;
@@ -15,13 +16,13 @@ import static java.nio.file.StandardOpenOption.*;
 import chariot.Client;
 import chariot.ClientAuth;
 import chariot.model.*;
-import chariot.model.Player.User;
 
 public class DownloadGames {
 
     static Path win  = Path.of("games-win.pgn");
     static Path draw = Path.of("games-draw.pgn");
     static Path loss = Path.of("games-loss.pgn");
+    static Path other = Path.of("games-other.pgn");
 
     public static void main(String[] args) throws Exception {
 
@@ -41,7 +42,7 @@ public class DownloadGames {
         //launchHttpServer();
 
         var counter = new LongAdder();
-        Consumer<Game> progress = __ -> {
+        Consumer<Pgn> progress = __ -> {
             counter.increment();
             if (counter.intValue() % 100 == 0) System.out.println("Downloaded %d games".formatted(counter.intValue()));
         };
@@ -54,25 +55,24 @@ public class DownloadGames {
         System.out.println("Downloading games from %s to %s".formatted(DateTimeFormatter.ISO_LOCAL_DATE.format(since), DateTimeFormatter.ISO_LOCAL_DATE.format(until)));
 
         System.out.println("Downloaded %d games".formatted(counter.intValue()));
-        client.games().byUserId(userId, parameters -> parameters
-                .pgn()
+        client.games().pgnByUserId(userId, parameters -> parameters
                 .clocks()
                 .since(since)
                 .until(until)
                 )
             .stream()
             .peek(progress)
-            .map(game -> new PgnFile(game.pgn(), destinationFile(game, userId)))
+            .map(pgn -> new PgnFile(pgn.toString(), destinationFile(pgn, userId)))
             .forEach(pgnFile -> writeToFile(pgnFile.file(), pgnFile.pgn()));
         System.out.println("Downloaded %d games".formatted(counter.intValue()));
     }
 
-    static Path destinationFile(Game game, String myId) {
-        if (game.winner() == null) return draw;
-        return switch(game.winner()) {
-            //case null  -> draw; // codespace vscode run doesn't like null in switch, so above workaround
-            case white -> game.players().white() instanceof User u && u.id().equals(myId) ? win : loss;
-            case black -> game.players().black() instanceof User u && u.id().equals(myId) ? win : loss;
+    static Path destinationFile(Pgn pgn, String myId) {
+        return switch(pgn.tagMap().get("Result")) {
+            case "1/2-1/2" -> draw;
+            case "0-1"     -> Objects.equals(myId, pgn.tagMap().get("White")) ? loss : win;
+            case "1-0"     -> Objects.equals(myId, pgn.tagMap().get("White")) ? win : loss;
+            default        -> other;
         };
     }
 
